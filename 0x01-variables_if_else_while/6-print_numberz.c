a4e86f6c6dbc27c5dbb5bb2860072d4c6837adef
Ashenafibe <Abekele119@gmail.com>
2022-09-09 16:54:39 -0700

    Numbers put

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print if number is positive, zero or negative
+ *
+ * description:using the main function print the number
+ * is positive, zero or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
