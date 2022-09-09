1cdcd125902b78fcfdf71ccc6eea433a9e870faa
Ashenafibe <Abekele119@gmail.com>
2022-09-09 14:52:04 -0700

    Positive anything is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,7 @@
 /**
  * main - print if number is positive, zero or negative
  *
- * description:using the main function 
- * thii programm print the number is positive ,zero or negative
+ * description:using the main function print the number is positive, zero or negative
  * Return:0 
  */
 int main(void)
