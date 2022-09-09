30fd35979bc6fba7fcceb101706896538de1dc79
Ashenafibe <Abekele119@gmail.com>
2022-09-09 16:10:07 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if number is positive, zero or negative
+ *
+ * description:using the main function print the number
+ * is positive, zero or negative
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	       putchar(ch);
+	}	       
+	putchar('\n');
+	return (0);
+}
