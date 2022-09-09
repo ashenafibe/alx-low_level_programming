dcd7f648612b71e156ffceb768793fa4e8c03996
Ashenafibe <Abekele119@gmail.com>
2022-09-09 16:23:04 -0700

    When I was having

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if number is positive, zero or negative
+ *
+ * description:using the main function print the number
+ * is positive, zero or negative
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'a' && ch != 'z')
+		{
+		putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
