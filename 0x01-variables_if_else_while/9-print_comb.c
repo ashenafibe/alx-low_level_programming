f56cc015e305186a7350a642967982c3a21f6397
Ashenafibe <Abekele119@gmail.com>
2022-09-09 18:53:54 -0700

    the

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+ * main - print  base 16 numbers
+ *
+ * description:using the main function print the number
+ * is base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d !=0)
+		{
+			putchar(',');
+			putchar(' ');
+
+		}
+                
+			
+			
+	}
+  
+	putchar('\n');
+	return (0);
+}
