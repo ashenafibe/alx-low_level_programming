931a2242a1c463bc31e5c4414397771639ae32ce
Ashenafibe <Abekele119@gmail.com>
2022-09-09 18:39:39 -0700

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - print if number is positive, zero or negative
+ * main - print if number is positiive, zero or negative
  *
  * description:using the main function print the number
  * is positive, zero or negative
