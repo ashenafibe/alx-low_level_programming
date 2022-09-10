5fac42494b4b765a067c15efb25664cd43bf8ef5
Ashenafibe <Abekele119@gmail.com>
2022-09-09 17:47:19 -0700

    Patience, persistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print combination of numbers
+ *
+ * description:using the main function print the combination 
+ * of numbers in 0 and 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i !='9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar("\n");
+	return (0);
+}
