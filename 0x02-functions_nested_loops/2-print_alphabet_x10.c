bc1259f9e072b67ea73ad0c9788f24689c735190
Ashenafibe <Abekele119@gmail.com>
2022-09-13 19:16:26 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 *Return: Always 0.
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 
 	char c, i;
 	
 	for (i = 0; i <= 9; i++)
 	{
-		for (c = 'a'; c <= 'z; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar('c');
 		}
