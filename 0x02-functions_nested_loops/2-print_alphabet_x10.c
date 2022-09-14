2271f5edc83af244217c8ca71e72ff492d8f9385
Ashenafibe <Abekele119@gmail.com>
2022-09-13 18:49:24 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*
+*main - check the code
+*
+*Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+
+	char c, i;
+	
+	for (0 = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; z++)
+		{
+			_putchar('c');
+		}
+	}
+	_putchar('\n');
+}
