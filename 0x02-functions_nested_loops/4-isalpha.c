9138b9a775482eb70740909bce53ec6de2f9f0b6
Ashenafibe <Abekele119@gmail.com>
2022-09-13 19:59:07 -0700

    islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,12 @@
  * _isalpha - a function to check lower case character
  * @c : is parameter
  *
+ * Description :print val
  * Return : 0
  */
 int _isalpha(int c)
 {
-	if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
