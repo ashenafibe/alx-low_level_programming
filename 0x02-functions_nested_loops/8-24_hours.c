90382d8cba9198fd63c6220a178668c548ae8dad
ashenafibe <Abekele119@gmail.com>
2022-09-14 05:25:25 -0700

    I am  federal agent

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer:
+ * jack_bauer:find the max num
  * 
  * Return : 0
  */
@@ -8,14 +8,14 @@ void jack_bauer(void)
 {
 	int hours = 0;
 	int minut = 0;
-	int hr_rm , min_rm;
+	int hr_rm, min_rm;
 
 	while (hours <= 23)
 	{
 		while (minut <= 59)
 		{
 			hr_rm = hours % 10;
-			min_rm =minut % 10;
+			min_rm = minut % 10;
 			_putchar(hours / 10 + '0');
 			_putchar(hr_rm + '0');
 			_putchar(':');
