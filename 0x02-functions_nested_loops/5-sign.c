e3b5c13ae85bd1af20e8c391d84dcb5fb54001d7
Ashenafibe <Abekele119@gmail.com>
2022-09-14 02:02:29 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,6 @@
  * print_sign - a function chech the sign of num
  * @n : is the int that will use for the argument of the function
  *
- * Description : print + if num is greater than 0 
  * Return : 0
  */
 int print_sign(int n)
