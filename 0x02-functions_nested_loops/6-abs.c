dc9a27cd9348aa794e831355835e0f23ac776957
Ashenafibe <Abekele119@gmail.com>
2022-09-14 02:27:43 -0700

    There is no such thing

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - the function compute the absolute value
+ * @n :the argument of function
+ *
+ * Return :0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else 
+	{
+		return (n*-1);
+	}
+}	
+
+
