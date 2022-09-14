f4b23bf946889e3244023dde4b593e9c57dc00ea
ashenafibe <Abekele119@gmail.com>
2022-09-14 05:16:10 -0700

    I am  federal agent

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer:
+ * 
+ * Return : 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minut = 0;
+	int hr_rm , min_rm;
+
+	while (hours <= 23)
+	{
+		while (minut <= 59)
+		{
+			hr_rm = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hr_rm + '0');
+			_putchar(':');
+			_putchar(minut / 10 + '0');
+			_putchar(min_rm + '0');
+			minut++;
+			_putchar('\n');
+		}
+		hours++;
+		minut = 0;
+	}
+}
