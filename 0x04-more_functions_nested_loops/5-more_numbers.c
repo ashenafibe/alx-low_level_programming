c42240f8513bb54d88eb787d5311e7218ff147cb
Abekele <Adibeto1984@gmail.com>
2022-09-15 07:11:15 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - more numbers
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int n,i;
+
+        while (i < 10)
+	{
+		for ( n = 0; n <= 14; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n')
+}
