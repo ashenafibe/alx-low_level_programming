edefbaf65c648b9d9ceca70ac05b412ac826f68c
Abekele <Adibeto1984@gmail.com>
2022-09-15 05:39:36 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * _isdigit - the function n is digit or no
+ * @c: argument of the function
+ *
+ * Return: 1  if it id digit 0 for not
+ */
+int _isdigit(char c)
+ {
+	 if (c >= '0' && c <= '9')
+	 {
+		 return (1);
+	 }
+	 else
+	 {
+		 return (0);
+	 }
+ }
