79178975eb7d18d0e42445524c2b913b0cac6d81
Abekele <Adibeto1984@gmail.com>
2022-09-19 07:18:17 -0700

    A good engineer thinks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverse string
+ * s: parameters of
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int length, c;
+	char *begin, *end, temp;
+
+	length = _strlen(s);
+	begin  = s;
+	end    = s;
+
+	for (c = 0; c < length - 1; c++)
+		end++;
+
+	for (c = 0; c < length/2; c++)
+	{
+		temp   = *end;
+		*end   = *begin;
+		*begin = temp;
+		 begin++;
+		 end--;
+	}
+}
