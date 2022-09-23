4a9974b7933131a56ad063c1764e8455f8774807
ashenafibe <Abekele119@gmail.com>
2022-09-23 04:37:33 -0700

    Half the lies they tell about me arenot true

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
-#endif
+void print_buffer(char *b, int size);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+void print_number(int n);
