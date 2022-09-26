79f86f436d300946544337bfd3fa8872357da2a5
Abekele <Adibeto1984@gmail.com>
2022-09-26 06:25:59 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,17 +4,15 @@
  *  _strchr - Locates a character in a string.
  *  @s: The string to be searched
  *  @c: The character to be located
- *  Return: If c is found - a pointer to the first occurence. 
+ *  Return: If c is found - a pointer to the first occurence.
  */
-char *_strchr(char *s, char c) 
+char *_strchr(char *s, char c)
 {
-	int index; 
-
-	for (index = 0; s[index] >= '\0'; index++)
+	for (int i = 0; s[i] >= '\0'; i++)
 	{
-		if (s[index] == c)
-			return (s + index);
+		if (s[i] == c)
+			return (s + i);
 	}
-	
-	return ('\0'); 
+
+	return ('\0');
 }
