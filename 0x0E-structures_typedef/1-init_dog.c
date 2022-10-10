dec2a0a878a2204ce5d41270cc415f1e4fa9cecc
ashenafibe <Abekele119@gmail.com>
2022-10-10 08:11:45 -0700

    num1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,8 @@
 
 /**
  * init_dog - Initializes a dog structure
- * @d: A dog structure.@name: The name of the dog.
+ * @d: A dog structure.
+ * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  *
