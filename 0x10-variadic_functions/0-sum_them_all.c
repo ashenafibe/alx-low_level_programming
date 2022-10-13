dcfe28874da10bec82b56445d1eaeff445f7c77d
Abekele <Adibeto1984@gmail.com>
2022-10-13 05:50:18 -0700

    . It is not the mountains ahead to

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - The sum all numbers
@@ -9,22 +10,18 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	int sum = 0;
+	unsigned int sum = 0, i;
 
-	if (n != 0)
-	{
-		va_start(args, n);
+	if (n == 0)
+		return (0);
 
-		while (i < n)
-		{
-			sum += va_arg(args, int);
-			i++;
-		}
-		
-		va_end(args);
-		return (sum);
-	}
+	va_start(valist, n);
 
-	return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+
 }
