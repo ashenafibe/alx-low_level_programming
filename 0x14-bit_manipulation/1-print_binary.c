f9803f052d2f46bc30cede2a474c8db00f50e735
Abekele <Adibeto1984@gmail.com>
2022-10-27 04:38:21 -0700

    bitmap

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * prints the binary representation of a number
+ * @n: number that to be convert
+ *
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int c = 0;
+	int bit = sizeof(n) * 8 - 1;
+
+	if (n == 0)
+		_putchar('0');
+
+	while (bit >= 0)
+	{
+		if (n >> bit & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+		bit--;
+	}
+}
